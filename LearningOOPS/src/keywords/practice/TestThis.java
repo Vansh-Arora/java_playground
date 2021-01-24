d9a8b491f72328c322eb2196c20add2bc5a65517
Vansh <vansharora2929@gmail.com>
2021-01-25 00:24:46 +0530

    keywords

---
+++ LearningOOPS/src/keywords/practice/TestThis.java
@@ -0,0 +1,10 @@
+package keywords.practice;
+
+public class TestThis {
+	public static void main(String args[]) {
+		ThisClass obj = new ThisClass(10,20);
+		obj.display();
+		ThisClass obj2 = new ThisClass(10,20,30);
+		obj2.display();
+	}
+}
--- LearningOOPS/src/keywords/practice/ThisClass.java
+++ LearningOOPS/src/keywords/practice/ThisClass.java
@@ -3,9 +3,16 @@ package keywords.practice;
 public class ThisClass {
 	int a;
 	int b;
+	int c;
 	ThisClass(int a, int b){
 		this.a = a;
 		this.b = b;
 	}
-
+	ThisClass(int a, int b, int c){
+		this(a,b);
+		this.c = c;
+	}
+	void display() {
+		System.out.println(a + " " + b + " " + c);
+	}
 }
