fd5128689a7cd7ffc30e9e93b7bd5cbf3289a69f
Vansh <vansharora2929@gmail.com>
2021-01-28 22:41:51 +0530

    Multiple inheritance

--- LearningOOPS/src/abstraction/KTM.java
+++ LearningOOPS/src/abstraction/KTM.java
@@ -1,12 +1,17 @@
 package abstraction;
-
-public class KTM implements Engine{
+// Multiple Inheritance
+public class KTM implements Engine,Brake{
 	public void start() {
 		System.out.println(power + "cc Engine started");
+	}
+	
+	public void applyBrake() {
+		System.out.println("Brakes applied");
 	}
 	
 	public static void main(String args[]) {
 		KTM duke = new KTM();
 		duke.start();
+		duke.applyBrake();
 	}
 }
