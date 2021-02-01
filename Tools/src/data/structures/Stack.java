db982598f6121f83a3274fe8eee670035af7fe75
Vansh <vansharora2929@gmail.com>
2021-02-01 23:17:50 +0530

    Stack

---
+++ Tools/src/data/structures/Stack.java
@@ -0,0 +1,54 @@
+package data.structures;
+
+abstract class Stack {
+	
+	int top;
+	int stack[] = new int[10];
+	
+	Stack() {
+		top = -1;
+	}
+	
+	boolean isFull() {
+		if(top == 9)
+			return true;
+		else
+			return false;
+	}
+	
+	boolean isEmpty() {
+		if(top == -1)
+			return true;
+		else 
+			return false;
+	}
+	
+	void push(int element) {
+		if(isFull()) {
+			System.out.println("Stack full");
+		}
+		else {
+			stack[++top] = element;
+		}
+	}
+	
+	void pop() {
+		if(isEmpty()) {
+			System.out.println("Stack empty");
+		}
+		else {
+			System.out.println(stack[top--]);
+		}
+	}
+	void print() {
+		if(isEmpty()) {
+			System.out.println("Stack empty");
+		}
+		else {
+			for(int i=0;i<=top;i++) {
+				System.out.println(stack[i]);
+			}
+		}
+	}
+
+}
---
+++ Tools/src/math/functions/binaryConversion.java
@@ -0,0 +1,10 @@
+package math.functions;
+
+public class binaryConversion {
+	/*public static String intToBinary(int num) {
+		
+	}*/
+	public static void main(String[] args) {
+		System.out.println(java.lang.Integer.toBinaryString(2));
+	}
+}
